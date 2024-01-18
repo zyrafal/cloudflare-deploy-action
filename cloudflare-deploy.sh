319a3a61f17cfec686c2a660513eb6d726e61154
アレクサンダー.eth <gpg@pavlovcik.com>
2024-01-18 21:21:02 +0900

    fix: deploy shell script

--- cloudflare-deploy.sh
+++ cloudflare-deploy.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-git clone https://github.com/${1} .
+# Get the repository, production branch, and output directory from the command line arguments
+repository=$1
+production_branch=$2
+output_directory=$3
+
+# Clone the repository
+git clone "https://github.com/${repository}.git"
+cd "$(basename "${repository}")" || exit
+
+# Install Node.js and Yarn
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
@@ -8,16 +17,26 @@ nvm install 20.10.0
 nvm use 20.10.0
 node --version
 yarn install
+
+# Build the project
 yarn build
+
+# Install Wrangler
 yarn add wrangler
-IFS='/' read -ra fields <<< "${1}"
+
+# Get the project name by replacing '.' with '-' in the repository name
+IFS='/' read -ra fields <<<"$repository"
 projectName="${fields[1]//./-}"
-echo $projectName
-yarn wrangler pages project list > project_list.txt
-if grep -q $projectName project_list.txt; then
+echo "$projectName"
+
+# Check if the project already exists
+yarn wrangler pages project list >project_list.txt
+if grep -q "$projectName" project_list.txt; then
   echo "Project found"
 else
   echo "Project not found"
-  yarn wrangler pages project create "$projectName" --production-branch "${2}"
+  yarn wrangler pages project create "$projectName" --production-branch "$production_branch"
 fi
-yarn wrangler pages deploy "${3}" --project-name "$projectName"
+
+# Deploy the project
+yarn wrangler pages deploy "$output_directory" --project-name "$projectName"
