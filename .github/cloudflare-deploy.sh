e18c854327433ba5442b9ea3be67e38b4e51853c
アレクサンダー.eth <gpg@pavlovcik.com>
2024-02-20 03:20:28 +0900

    chore: debug in prod

--- .github/cloudflare-deploy.sh
+++ .github/cloudflare-deploy.sh
@@ -1,31 +1,20 @@
 #!/bin/bash
 set -e
+PROJECT=$1
+DEFAULT_BRANCH=$2
+DIST=$3
+CURRENT_BRANCH=$4
 
-# Get the repository, production branch, and output directory from the command line arguments
-repository=$1
-productionBranch=$2
-builtProjectDirectory=$3
-
-# Extract the organization name and repository name from the repository variable
-IFS='/' read -ra fields <<<"$repository"
-# organizationName="${fields[0]}"
-repositoryName="${fields[1]}"
-
-repositoryName=${repositoryName//./-}
-
-# Change directory to the repository root
-cd "$repository" || exit
+IFS='/' read -ra fields <<<"$PROJECT"
+REPOSITORY_NAME="${fields[1]}"
+REPOSITORY_NAME=${REPOSITORY_NAME//./-}
+cd "$PROJECT" || exit
 
 yarn add wrangler -d --frozen-lockfile
-# wrangler_path=$(yarn bin)/wrangler # /opt/hostedtoolcache/node/20.10.0/x64/bin/wrangler
-# export PATH="$PATH:$wrangler_path"
-
-if ! yarn wrangler pages project list | grep -q "$repositoryName"; then
-  echo "Project not found"
-  yarn wrangler pages project create "$repositoryName" --production-branch "$productionBranch"
+if ! yarn wrangler pages project list | grep -q "$REPOSITORY_NAME"; then
+  yarn wrangler pages project create "$REPOSITORY_NAME" --production-branch "$DEFAULT_BRANCH"
 fi
 
-output_url=$(yarn wrangler pages deploy "$builtProjectDirectory" --project-name "$repositoryName" --branch "$productionBranch" --commit-dirty=true)
-
+output_url=$(yarn wrangler pages deploy "$DIST" --project-name "$REPOSITORY_NAME" --branch "$CURRENT_BRANCH" --commit-dirty=true)
 output_url="${output_url//$'\n'/%0A}"
 echo "DEPLOYMENT_URL=$output_url" >>"$GITHUB_ENV"
