f7107714ceb2b769cccbde10f232f72d367ca192
アレクサンダー.eth <gpg@pavlovcik.com>
2024-02-20 02:08:19 +0900

    chore: debug in prod

--- .github/cloudflare-deploy.sh
+++ .github/cloudflare-deploy.sh
@@ -18,19 +18,6 @@ cd "$organizationName/$repositoryName" || exit
 yarn add tsx wrangler -d --frozen-lockfile
 wrangler_path=$(yarn bin)/wrangler # /opt/hostedtoolcache/node/20.10.0/x64/bin/wrangler
 export PATH="$PATH:$wrangler_path"
-yarn wrangler pages --help
-
-# find ../../.. -type d \( -name "node_modules" -o -name ".git" -o -name ".husky" \) -prune -o -print
-
-account_id="17b9dfa79e16b79dffcb11a66768539c" # Ubiquity DAO Workers
-
-wrangler_file_path="../wrangler.toml"
-
-if grep -q "account_id" "$wrangler_file_path"; then
-  sed -i "s/account_id = \".*\"/account_id = \"$account_id\"/g" "$wrangler_file_path"
-else
-  echo "account_id = \"$account_id\"" >>"$wrangler_file_path"
-fi
 
 if yarn wrangler pages project list | grep -q "$repositoryName"; then
   echo "Project found"
@@ -38,6 +25,9 @@ else
   echo "Project not found"
   yarn wrangler pages project create "$repositoryName" --production-branch "$productionBranch"
 fi
+
+yarn wrangler --version
+npx wrangler --version
 
 if [ "$productionBuild" = "true" ]; then
   output_url=$(npx wrangler pages deploy "$builtProjectDirectory" --project-name "$repositoryName" --commit-dirty=true)
