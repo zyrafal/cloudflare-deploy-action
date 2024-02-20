06f344e2d636e42e99cce98ed679cf0795d7f999
アレクサンダー.eth <gpg@pavlovcik.com>
2024-02-20 21:40:22 +0900

    feat: locally sourced creds

--- post-deployment.ts
+++ post-deployment.ts
@@ -1,6 +1,4 @@
-import { config } from "dotenv";
 import { postDeployment } from "./src/post-deployment";
-config();
 
 postDeployment()
   .then(() => console.log("Post deployment tasks have been completed."))
